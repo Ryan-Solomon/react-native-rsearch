7f867256991ceb34cb3b333c1fe7457f32fbf34f
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-06 16:54:05 -0600

    setup api request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [submitTerm, setSubmitTerm] = useState('');
+
+  React.useEffect(() => {
+    const getResults = async () => {
+      const { data } = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      });
+      setResults(data.businesses);
+    };
+    getResults();
+  }, [submitTerm]);
 
   return (
     <View>
       <SearchBar
-        onTermSubmit={() => ''}
+        onTermSubmit={() => setSubmitTerm(term)}
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
       />
-      <Text>Search Screen Test</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
